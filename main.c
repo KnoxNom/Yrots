9e2bc491355b4c46d99a0095f25ccbc54e2b6ce2
Knox <105970239+KnoxNom@users.noreply.github.com>
2022-05-31 17:54:06 +0200

    Story.txt
    The Ends...

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 int main() {
    printf("Yrots\n");
    printf("Ah ah ah one dracula, ah ah ah two dracula's, ah ah ah three dracula's ah ah ah\n");
-
+   // Story.txt
+   printf("EEEE\n");
+   printf("hi, this guy is balckmaling and making me do some wired stuffs i mean nvm huii i am someon idk what to do and yee...\n");
+   printf("The End...");
+   printf("Wang\n");
+   printf("i was told to write something here...\n");
+   printf("The Ends...\n"); // No, I didn't make a spelling mistake it is "The Ends..." just go check :D
+   
    return 0;
 }
